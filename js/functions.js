5927edf430081a998ad0216cae91df7a5dbba53e
Hannu Juopperi <t1juha00@students.oamk.fi>
2024-03-06 21:39:22 +0200

    Säätietojen hakeminen avoimen datan palvelusta

---
+++ js/functions.js
@@ -0,0 +1,46 @@
+const temp_span = document.querySelector('#temp');
+const speed_span = document.querySelector('#speed');
+const direction_span = document.querySelector('#direction');
+const description_span = document.querySelector('#description');
+const icon_img = document.querySelector('.image-wrapper img');
+const url ='https://api.openweathermap.org/data/2.5/weather?';
+const icon_url = 'https://openweathermap.org/img/wn/';
+const api_key = '';
+
+const getLocation = () => {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(position => {
+            const lat = position.coords.latitude.toFixed(3);
+            const lng = position.coords.longitude.toFixed(3);
+            document.querySelector('#lat').innerHTML = lat + ', ';
+            document.querySelector('#lng').innerHTML = lng;
+            getWeather(lat, lng);
+        }, error => {
+            alert('Error: ' + error.message);
+        });
+    } else {
+        alert("Your browser does not support geolocation!");
+    }
+};
+
+
+const getWeather = (lat,lng) => {
+    console.log("Latitude:", lat, "Longitude:", lng);
+    const address = url + 'lat=' + lat + '&lon=' + lng + '&units=metric' + '&appid=' + api_key;
+    axios.get(address)
+    
+        .then(response => {
+            const json = response.data
+            temp_span.innerHTML = json.main.temp + '&#8451;'
+            speed_span.innerHTML = json.wind.speed + ' m/s'
+            direction_span.innerHTML = json.wind.deg + '&#176;'
+            description_span.innerHTML = json.weather[0].description
+            const image = icon_url + json.weather[0].icon + '@2x.png'
+            icon_img.src = image
+        }).catch(error => {
+            alert(error)
+        })
+}
+
+
+getLocation();
